72e3b4ef2be17e228e713db846fca007c319eef5
CandraW <wijayacandra048@gmail.com>
2026-03-22 11:03:05 +0800

    add missing sprites and separate script from scene

---
+++ scenes/hero.gd
@@ -0,0 +1,51 @@
+extends CharacterBody2D
+
+@export_category("Stats")
+@export var speed : int = 480
+var move_direction : Vector2 = Vector2.ZERO
+
+@onready  var animated_sprite := $AnimatedSprite2D
+
+
+func _physics_process(_delta: float) -> void:
+	movement_loop()
+ 
+func _ready() -> void:
+	pass
+	
+func _process(_delta: float) -> void:
+	pass
+
+func movement_loop() -> void:
+	
+	move_direction = Input.get_vector("left", "right", "up", "down")
+	
+	animated_sprite.flip_h = (
+		animated_sprite.flip_h 
+		if move_direction.x == 0 
+		else true if move_direction.x < 0 
+		else false
+	)
+
+	#match move_direction:
+		#Vector2.RIGHT:
+			#sprite_direction = "right"
+			#animated_sprite.flip_h = false
+		#Vector2.LEFT:
+			#sprite_direction = "right"
+			#animated_sprite.flip_h = true			
+		#Vector2.UP:
+			#sprite_direction = "up"		
+		#Vector2.DOWN:
+			#sprite_direction = "down"
+	
+	play_move_animation("down")
+	
+	velocity = move_direction * speed
+	move_and_slide()
+	
+func play_move_animation(direction) -> void:
+	if move_direction != Vector2.ZERO:
+		animated_sprite.play("move_" + direction)
+	else:
+		animated_sprite.play("idle_" + direction)
