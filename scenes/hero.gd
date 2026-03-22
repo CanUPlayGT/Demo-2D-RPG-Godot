ecebb4c9a6876ad5598fc3a386fb589ee726aa10
CandraW <wijayacandra048@gmail.com>
2026-03-22 15:29:37 +0800

    sprites and world scene rework

--- scenes/hero.gd
+++ scenes/hero.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 @export var speed : int = 480
 var move_direction : Vector2 = Vector2.ZERO
 
+# Initial sprite direction
+var sprite_direction = "down"
 @onready  var animated_sprite := $AnimatedSprite2D
 
 
@@ -17,29 +19,30 @@ func _process(_delta: float) -> void:
 	pass
 
 func movement_loop() -> void:
-	
 	move_direction = Input.get_vector("left", "right", "up", "down")
 	
+	# This is just ternary operator
+	# to give flip_h value based on 
+	# input direction (left or right) 
 	animated_sprite.flip_h = (
 		animated_sprite.flip_h 
 		if move_direction.x == 0 
 		else true if move_direction.x < 0 
 		else false
-	)
-
-	#match move_direction:
-		#Vector2.RIGHT:
-			#sprite_direction = "right"
-			#animated_sprite.flip_h = false
-		#Vector2.LEFT:
-			#sprite_direction = "right"
-			#animated_sprite.flip_h = true			
-		#Vector2.UP:
-			#sprite_direction = "up"		
-		#Vector2.DOWN:
-			#sprite_direction = "down"
+	) 
+	# flip horizontally if input direction is left
 	
-	play_move_animation("down")
+	match move_direction:
+		Vector2.RIGHT:
+			sprite_direction = "right"
+		Vector2.LEFT:
+			sprite_direction = "right"
+		Vector2.UP:
+			sprite_direction = "up"		
+		Vector2.DOWN:
+			sprite_direction = "down"
+	
+	play_move_animation(sprite_direction)
 	
 	velocity = move_direction * speed
 	move_and_slide()
